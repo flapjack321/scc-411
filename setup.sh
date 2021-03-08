f7706ee6ded3aeea40e48b98e5292c038a85d48b
Ethan Cotterell <31697174+flapjack321@users.noreply.github.com>
2021-03-08 11:04:34 +0000

    Removed sudo requirements from setup

--- setup.sh
+++ setup.sh
@@ -29,86 +29,33 @@ function copy-as-hive() {
     chmod 644 $HIVE_DIRECTORY/conf/$1
 }
 
+function copy_bashrc() {
+    cp $(pwd)/bashrc ~/.bashrc
+    chmod 644 ~/.bashrc
+}
 
 ###
-### System Admin
+### Admin Tasks
 ###
-
-# delete the hadoop user
-if id "$HADOOP_USER" &>/dev/null; then
-    if [ -f "/home/$HADOOP_USER/.ssh/id_rsa" ]; then
-        HAS_SSH_KEY=1
-        # We need to copy out the keys
-        cp /home/$HADOOP_USER/.ssh/id_rsa /tmp/hadoop_ssh_key
-        cp /home/$HADOOP_USER/.ssh/id_rsa.pub /tmp/hadoop_ssh_key.pub
-        # Copy out authorized_keys so ssh-copy-id doesn't need to be rerun on nodes
-        cp /home/$HADOOP_USER/.ssh/authorized_keys /tmp/hadoop_authorized_keys
-    fi
-
-    deluser $HADOOP_USER
-    rm -rf /home/$HADOOP_USER
-fi
-
-
-# create a new hadoop user
-useradd -m \
-    -p "$(python3 -c 'import crypt; print(crypt.crypt("hadoop"))')" \
-    -s /bin/bash \
-    $HADOOP_USER
+echo "Killing existing hadoop/hive processes"
+kill -9 $(jps | cut -d' ' -f1)
 
 ###
-### Install hadoop
+### Install Hadoop
 ###
 
-# generate SSH keys
-if [ "$HAS_SSH_KEY" -eq "0" ]; then
-    # No ssh key present so generate one
-    su $HADOOP_USER <<EOF
-set -e -x
-mkdir ~/.ssh
-ssh-keygen -t rsa -f ~/.ssh/id_rsa -q -N ''
-cat ~/.ssh/id_rsa.pub > ~/.ssh/authorized_keys
-EOF
-else
-    # SSH was present so copy from tmp
-    mkdir /home/$HADOOP_USER/.ssh
-    cp /tmp/hadoop_ssh_key /home/$HADOOP_USER/.ssh/id_rsa
-    chmod 600 /home/$HADOOP_USER/.ssh/id_rsa
-    cp /tmp/hadoop_ssh_key.pub /home/$HADOOP_USER/.ssh/id_rsa.pub
-    chmod 644 /home/$HADOOP_USER/.ssh/id_rsa.pub
-    cp /tmp/hadoop_authorized_keys /home/$HADOOP_USER/.ssh/authorized_keys
-    chmod 664 /home/$HADOOP_USER/.ssh/authorized_keys
-
-    chown -R $HADOOP_USER:$HADOOP_USER /home/$HADOOP_USER/.ssh
-fi
-
-
-# download hadoop
-su $HADOOP_USER <<EOF
-set -e -x
-mkdir ~/downloads
+mkdir -p ~/downloads
 wget --quiet -O ~/downloads/$HADOOP_TARBALL $HADOOP_URL
 tar -zxf ~/downloads/$HADOOP_TARBALL -C ~/
 
-
-# add env variables
-echo "export HADOOP_HOME=$HADOOP_DIRECTORY" >> ~/.bashrc
-echo "export HADOOP_INSTALL=$HADOOP_DIRECTORY" >> ~/.bashrc
-echo "export HADOOP_COMMON_HOME=$HADOOP_DIRECTORY" >> ~/.bashrc
-echo "export HADOOP_MAPRED_HOME=$HADOOP_DIRECTORY" >> ~/.bashrc
-echo "export HADOOP_HDFS_HOME=$HADOOP_DIRECTORY" >> ~/.bashrc
-echo "export HADOOP_COMMON_LIB_NATIVE=$HADOOP_DIRECTORY/lib/native" >> ~/.bashrc
-echo "export YARN_HOME=$HADOOP_DIRECTORY" >> ~/.bashrc
-echo "export HADOOP_OPTS=\"-Djava.library.path=$HADOOP_DIRECTORY/lib/native\"" >> ~/.bashrc
-echo "export PATH=$PATH:$HADOOP_DIRECTORY/sbin:$HADOOP_DIRECTORY/bin" >> ~/.bashrc
-
-# create extra directories
+# remove and recreate directories
+rm -rf ~/hadooptempdata
 mkdir ~/hadooptempdata
+
+rm -rf ~/hdfs
 mkdir ~/hdfs
 mkdir ~/hdfs/namenode
 mkdir ~/hdfs/datanode
-
-EOF
 
 # copy hadoop configuration files in place
 # note: not perfect as versions etc. are hard coded in these files
@@ -118,41 +65,32 @@ copy-as-hadoop hdfs-site.xml
 copy-as-hadoop mapred-site.xml
 copy-as-hadoop yarn-site.xml
 
-# format the hadoop namenode
-su $HADOOP_USER <<EOF
-set -e -x
+###
+### Format HDFS namenode
+###
 PATH=$PATH:$HADOOP_DIRECTORY/bin hdfs namenode -format 1> /dev/null 2>&1
-echo "Exit code $?"
-EOF
+echo "Formatting HDFS namenode exit code $?"
 
 echo "Hadoop has been successfully setup. Run 'start-dfs.sh' and 'start-yarn.sh'"
-
-
-su $HADOOP_USER <<EOF
-set -e -x
-curl -fsSL https://data.cdc.gov/api/views/g4ie-h725/rows.csv\?accessType\=DOWNLOAD > ~/downloads/chronic_diseases.csv
-EOF
-
 
 ###
 ### Begin Hive install
 ###
-su $HADOOP_USER <<EOF
-set -e -x
 wget --quiet -O ~/downloads/$HIVE_TARBALL $HIVE_URL
 tar -zxf ~/downloads/$HIVE_TARBALL -C ~/
-
-echo "export HIVE_HOME=$HIVE_DIRECTORY" >> ~/.bashrc
-echo "export HIVE_CONF_DIR=/home/$HADOOP_USER/apache-hive-$HIVE_VERSION/conf" >> ~/.bashrc
-echo "export PATH=$PATH:$HADOOP_DIRECTORY/sbin:$HADOOP_DIRECTORY/bin:$HIVE_DIRECTORY/bin" >> ~/.bashrc
-EOF
 
 copy-as-hive hive-env.sh
 copy-as-hive hive-site.xml
 
-su $HADOOP_USER <<EOF
-set -e -x
+###
+### Setup Derby
+###
 cd $HIVE_DIRECTORY
 bin/schematool -initSchema -dbType derby 1> /dev/null 2>&1
 echo "Schema tools return code $?"
-EOF
+
+###
+### Copy configured bashrc
+###
+copy-bashrc
+source ~/.bashrc
